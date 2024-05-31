a81b3962cbd72444ba309e8871c7f726b9b08113
Mohammad Hamza <mohdhamzarehman@gmail.com>
2024-05-31 11:56:22 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+    const App = () => {
+      return (
+        <>
+          <div className="text-4xl flex shadow-lg rounded-md ">
+            React + Vite +Tailwind
+          </div>
+        </>
+      );
+    };
+    export default App;
+    
+    
